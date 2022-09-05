965d212c750c213080a7a32cea862014ebf1e67d
user <Klimovich.maks@gmail.com>
2022-09-05 23:19:01 +0300

    add task 54

---
+++ Task54/Program.cs
@@ -0,0 +1,65 @@
+﻿
+//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по 
+//убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//1 2 4 7
+//2 3 5 9
+//2 4 4 8
+
+
+int[,] GetMatrix(int n, int m)
+{
+    int[,] matrix = new int[n, m];
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] OrderingLinesMatrix(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, j] < matrix[i, k])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = GetMatrix(5, 5);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] newMatrix = OrderingLinesMatrix(matrix);
+PrintMatrix(newMatrix);
