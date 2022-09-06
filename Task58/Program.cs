92a00853bc701dd6d8c905e91f2b1f3ee7e89ada
user <Klimovich.maks@gmail.com>
2022-09-06 10:05:05 +0300

    add Home Task 58

---
+++ Task58/Program.cs
@@ -0,0 +1,65 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, заданы 2 массива:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//и
+//1 5 8 5
+//4 9 4 2
+//7 2 2 6
+//2 3 4 7
+//Их произведение будет равно следующему массиву:
+//1 20 56 10
+//20 81 8 6
+//56 8 4 24
+//10 6 24 49
+
+
+int[,] GetMatrix(int n, int m)
+{
+    int[,] matrix = new int[n, m];
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultiMatrix(int[,] matrixOne, int[,] matrixTwo)
+{
+    int[,] newMatrix = new int[matrixOne.GetLength(0), matrixOne.GetLength(1)];
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = (matrixOne[i, j] * matrixTwo[i, j]);
+        }
+    }
+    return newMatrix;
+}
+
+int[,] matrixOne = GetMatrix(4, 4);
+PrintMatrix(matrixOne);
+Console.WriteLine();
+int[,] matrixTwo = GetMatrix(4, 4);
+PrintMatrix(matrixTwo);
+Console.WriteLine();
+int[,] matrix = MultiMatrix(matrixOne, matrixTwo);
+PrintMatrix(matrix);
